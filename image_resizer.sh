440589ff031c88cef2dfd1ed329e3e1a56a5bb75
Damien Storm <damien.r.storm@gmail.com>
2023-04-22 13:15:46 -0400

    image_resizer.sh: Added command-line options and test mode
    - Added check for 'convert' command availability
    - Added usage function to display script options
    - Added command-line options to set desired dimensions and enable test mode
    - Added check for test mode to skip file conversion
    
    Commit message created with help from ChatGPT.

--- image_resizer.sh
+++ image_resizer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Define color codes
 WHITE='\033[1;37m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
@@ -6,10 +8,43 @@ GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 NC='\033[0m'
+SPACER="\n${WHITE}================================================================================${NC}\n"
 
+# Check if convert command is available
+if ! command -v convert &> /dev/null; then
+  echo -e "${RED}The 'convert' command is not available. Please install ImageMagick and try again.${NC}"
+  exit 1
+fi
 
+# Set default dimensions
 desired_dimensions="2560x1080"
-mkdir -p originals
+
+# Define usage function
+usage() {
+  echo "Usage: $0 [-t] [-d dimensions]"
+  echo "  -t: Enable test mode (skip file conversion)"
+  echo "  -d dimensions: Set desired dimensions (default: 1920x1080)"
+  exit 1
+}
+
+# Parse command-line options
+while getopts ":td:" opt; do
+  case $opt in
+    t) test_mode=1;;
+    d) desired_dimensions=$OPTARG;;
+    \?) echo "Invalid option -$OPTARG" >&2; usage;;
+    :) echo "Option -$OPTARG requires an argument" >&2; usage;;
+  esac
+done
+
+if [ "$test_mode" == "1" ]; then
+  echo -e "${SPACER}${YELLOW}Running in test mode. No changes will be made to existing fils${NC}${SPACER}"
+else
+  # Create originals directory if it doesn't exist
+  mkdir -p originals
+fi
+
+# Loop through image files
 for image_file in *.{jpg,jpeg,png}; do
   echo -e "\n${BLUE}$image_file${NC}"
   dimensions=$(identify -format '%wx%h' "$image_file")
@@ -21,9 +56,7 @@ for image_file in *.{jpg,jpeg,png}; do
     echo -e "  ${WHITE}Converting image from original dimensions ${YELLOW}$dimensions${WHITE} to ${GREEN}$desired_dimensions${NC}"
     new_filename="${filename}___resized_to_${desired_dimensions}.${extension}"
     echo -e "  ${WHITE}New filename: ${CYAN}$new_filename${NC}"
-    if [ "$1" == "test" ]; then
-      echo -e "${YELLOW}Skipping file conversion in test mode${NC}"
-    else
+    if [ "$test_mode" != "1" ]; then
       convert "$image_file" -resize "$desired_dimensions" -background black -gravity center -extent "$desired_dimensions" "$new_filename"
       mv "$image_file" "originals/$image_file"
     fi
