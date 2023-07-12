422db0dc68ac5576f005af2095d1428ca9b731fd
Damien Storm <damien.r.storm@gmail.com>
2023-07-12 13:22:37 -0400

    Updated chatgpt_commit_message_generator.rb
    - Added OPENAI_MODEL environment variable
    - Replaced hardcoded model with OPENAI_MODEL environment variable
    - Added OPENAI_URL constant
    - Moved API key and model checks to setup and functions section
    - Extracted headers and message body into separate functions
    - Refactored error handling in send_request_to_openai_api
    - Moved prompt initialization to main script section
    - Replaced hardcoded question with instance variable
    - Escaped double quotes and tildes in commit message
    - Removed redundant escape of double quotes in commit message

--- chatgpt_commit_message_generator.rb
+++ chatgpt_commit_message_generator.rb
@@ -13,6 +13,7 @@
 #   - Ruby 2.6 or higher
 #   - Bundler
 #   - OpenAI API key set in OPENAI_API_KEY environment variable
+#   - OpenAI model ID set in OPENAI_MODEL environment variable
 #
 # Usage:
 #  1. Stage changes you want to commit
@@ -30,6 +31,24 @@
 # Review OpenAI's Terms of Use at https://openai.com/policies/terms-of-use
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+# ==============================================================================
+# Setup and Functions
+# ==============================================================================
+
+OPENAI_URL = 'https://api.openai.com/v1/chat/completions'
+OPENAI_API_KEY = ENV['OPENAI_API_KEY']
+OPENAI_MODEL = ENV['OPENAI_MODEL']
+if OPENAI_API_KEY.nil? || OPENAI_MODEL.nil?
+  puts 'Please set the OPENAI_API_KEY and OPENAI_MODEL environment variables.'.red
+  exit 1
+end
+
+@staged_content = `git --no-pager diff --staged --unified=1`
+if @staged_content.empty?
+  puts 'No changes have been staged. Please stage changes before running this script.'.red
+  exit 1
+end
+
 require 'bundler/inline'
 
 gemfile do
@@ -45,111 +64,106 @@ require 'json'
 require 'ostruct'
 require 'time'
 
-def send_request_to_openai_api(question)
-  url = 'https://api.openai.com/v1/chat/completions'
-  headers = {
+def question
+  <<~QUESTION
+    I need you to create a commit message for me based on these guidelines:
+
+    - Use the past tense for the commit message.
+    - Include a subject line and a body with a bulleted list of more details.
+    - The subject line should be followed by a blank line
+    - The subject line should be a single line that is no longer than 50 characters
+    - If the changes only include 1 file, then the subject line should include the file name.
+    - The body should use bullets if appropriate.
+    - The lines in the body should wrap at 72 characters
+
+    Format your response as JSON with the following structure:
+    ```json
+    {
+      "subject": "<SUBJECT_LINE>",
+      "body": "<BODY>"
+    }
+    ```
+
+    Here are the differences for the commit:
+    ```#{@staged_content}```
+  QUESTION
+end
+
+def headers
+  {
     'Content-Type' => 'application/json',
     'Authorization' => "Bearer #{OPENAI_API_KEY}"
   }
-  body = {
-    "model": 'gpt-3.5-turbo',
-    "messages": [{ "role": 'user', "content": question }],
-    "temperature": 0.25
-  }
-  print 'Sending request to OpenAI API...'.white
-  response = nil
-  retries = 3
-  begin
-    response = HTTParty.post(url, headers: headers, body: body.to_json, timeout: 180)
-  rescue HTTParty::Error, Net::ReadTimeout => e
-    retries -= 1
-    raise e if retries.zero?
-
-    puts "\nRetry attempt #{3 - retries} of 3 failed!"
-    puts 'Encountered error while sending request to OpenAI API:'.yellow
-    puts "#{e.class}: #{e.message}".red
-    sleep rand(1..5)
-    retry
-  end
-  response
 end
 
-prompt = TTY::Prompt.new
-
-OPENAI_API_KEY = ENV['OPENAI_API_KEY']
-if OPENAI_API_KEY.nil?
-  puts 'Please set OPENAI_API_KEY environment variable'.red
-  exit 1
-end
-
-# Check if changes have been staged
-staged_content = `git --no-pager diff --staged --unified=1`
-if staged_content.empty?
-  puts 'No changes have been staged. Please stage changes before running this script.'.red
-  exit 1
-end
-
-puts "\n--------------------------------------------------------------------------------".white
-# Start timer
-start_time = Time.now
-
-# Form the prompt for ChatGPT
-question = <<~QUESTION
-  I need you to create a commit message for me based on these guidelines:
-
-  - Use the past tense for the commit message.
-  - Include a subject line and a body with a bulleted list of more details.
-  - The subject line should be followed by a blank line
-  - The subject line should be a single line that is no longer than 50 characters
-  - If the changes only include 1 file, then the subject line should include the file name.
-  - The body should use bullets if appropriate.
-  - The lines in the body should wrap at 72 characters
-
-  Format your response as JSON with the following structure:
-  ```json
+def message_body
+  formatted_question = question.gsub("\n", ' ')
   {
-    "subject": "<SUBJECT_LINE>",
-    "body": "<BODY>"
-  }
-  ```
-
-  Here are the differences for the commit:
-  ```#{staged_content}```
-QUESTION
-
-# Send request to OpenAI API
-response = send_request_to_openai_api(question)
-
-# Check for errors in the response
-if response['error']
-  print "✗\n\nOpenAI API Error: #{response['error']}\n".red
-  exit 1
+    "model": ENV['OPENAI_MODEL'],
+    "messages": [{ "role": 'user', "content": formatted_question }],
+    "temperature": 0.25
+  }.to_json
 end
-print "✓\n".green
 
-# Extract the generated commit message from the response
-json_response = response['choices'][0]['message']['content']
-parsed_response = JSON.parse(json_response, object_class: OpenStruct)
-message = <<~MESSAGE
-  #{parsed_response.subject}
+def send_request_to_openai_api
+  print 'Sending request to OpenAI API...'.white
+  retries = 3
+  HTTParty.post(OPENAI_URL, headers: headers, body: message_body, timeout: 180)
+rescue HTTParty::Error, Net::ReadTimeout => e
+  retries -= 1
+  raise e if retries.zero?
 
-  #{parsed_response.body}
+  puts "\nAttempt #{3 - retries} of 3 failed:".yellow
+  puts "#{e.class}: #{e.message}".red
+  sleep rand(1..5)
+  retry
+end
 
-  Commit message created with help from ChatGPT.
-MESSAGE
-Clipboard.copy(message) # Copy the generated commit message to the clipboard
-
-# End timer and display summary
-end_time = Time.now
-elapsed_time = end_time - start_time
-time_message = "\nTime to get message from ChatGPT: #{elapsed_time.round(2)} seconds"
-puts time_message.yellow
-puts "\nThe commit message has been copied to your clipboard and is displayed below".magenta
-puts "\nCommit message generated by ChatGPT:\n".white
-puts message.cyan
-puts "\n--------------------------------------------------------------------------------".white
+# ==============================================================================
+# Main Script
+# ==============================================================================
 
 begin
+  prompt = TTY::Prompt.new
+
+  puts "\n--------------------------------------------------------------------------------".white
+  # Start timer
+  start_time = Time.now
+
+  # Send request to OpenAI API
+  response = send_request_to_openai_api
+
+  # Check for errors in the response
+  if response['error']
+    print "✗\n\nOpenAI API Error: #{response['error']}\n".red
+    exit 1
+  end
+  print "✓\n".green
+
+  # Extract the generated commit message from the response
+  json_response = response['choices'][0]['message']['content']
+  parsed_response = JSON.parse(json_response, object_class: OpenStruct)
+
+  # Escape double quotes and tildes in the commit message
+  message = <<~MESSAGE
+    #{parsed_response.subject}
+
+    #{parsed_response.body.gsub('"', '\"').gsub('`', "'")}
+
+  MESSAGE
+
+  # Commit message created with help from ChatGPT.
+  Clipboard.copy(message) # Copy the generated commit message to the clipboard
+
+  # End timer and display summary
+  end_time = Time.now
+  elapsed_time = end_time - start_time
+  time_message = "\nTime to get message from ChatGPT: #{elapsed_time.round(2)} seconds"
+  puts time_message.yellow
+  puts "\nThe commit message has been copied to your clipboard and is displayed below".magenta
+  puts message.cyan
+  puts "\n--------------------------------------------------------------------------------".white
+
   # Prompt the user for how to proceed
   user_input = prompt.select("\nWhat would you like to do?") do |menu|
     menu.enum '.'
@@ -159,23 +173,25 @@ begin
     menu.choice 'Exit without committing', 3
     menu.choice 'Start a debugger session', 4
   end
+rescue StandardError => e
+  puts "\nEncountered an error:".yellow
+  puts "#{e.class}: #{e.message}".red
+  binding.pry
+  puts "\nExiting debugger session...".yellow
 rescue SystemExit, Interrupt
   # Gracefully handle exceptions like Ctrl-C or Ctrl-D
   puts "\nExiting without committing...".yellow
   exit 1
 end
 
-# Escape double quotes in the commit message
-escaped_message = message.gsub('"', '\"').gsub('`', '\`')
-
 # Process the user's input
 case user_input
 when 1
   puts "\nSubmitting commit...".white
-  system("git commit -m \"#{escaped_message}\"")
+  system("git commit -m \"#{message}\"")
 when 2
   puts "\nOpening editor...".white
-  system("git commit -e -m \"#{escaped_message}\"")
+  system("git commit -e -m \"#{message}\"")
 when 3
   puts "\nExiting without committing...".yellow
   exit 1
