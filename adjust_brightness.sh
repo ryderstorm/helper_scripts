7da25487cb8ae1ef0a2d552554065e874bcc68cf
Damien Storm <damien.r.storm@gmail.com>
2023-07-13 12:07:38 -0400

    Updated adjust_brightness.sh
    - Created log() and log_and_notify() functions for output logging and user notification
    - Replaced direct echo and notify-send calls with these new functions
    - Removed redundant echo statements
    - Added log statement at the start of the script

--- adjust_brightness.sh
+++ adjust_brightness.sh
@@ -20,17 +20,49 @@
 # can find the path by running the command "which ddcutil".
 # =============================================================================
 
+# Color codes for output
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+# Function that logs output to stdout with a timestamp
+log() {
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  echo -e "${WHITE}${timestamp}${NC} | $1"
+}
+
+# Function that logs output to stdout and notifies the user
+log_and_notify() {
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  notify_header="Brightness Control"
+  message="$1"
+  notify_status="${2:-info}"
+  if [ "$notify_status" = "error" ]; then
+    echo -e "${WHITE}${timestamp}${NC} | ${RED}$message${NC}"
+    notify_header="Brightness Control - Error"
+  elif [ "$notify_status" = "warning" ]; then
+    echo -e "${WHITE}${timestamp}${NC} | ${YELLOW}$message${NC}"
+    notify_header="Brightness Control - Warning"
+  else
+    echo -e "${WHITE}${timestamp}${NC} | $message"
+  fi
+  message=$(echo "$message" | awk '{gsub(/\x1b\[[0-9;]*m/,"")}1') # Strip color formatting
+  notify-send "$notify_header" "$message"
+}
+
 set_brightness() {
-  output=$(sudo ddcutil setvcp 10 "$1" 2>&1)
+  sudo ddcutil setvcp 10 "$1" 2>&1
 }
 
 process_input() {
   if [ -z "$user_param" ]; then
     message="You must specify a parameter, one of: \"increase\", \"decrease\", or a number from 30 to 100"
-    echo -e "\n${RED}$message${NC}"
-    notify-send "Brightness Control - Error" "$message"
+    log_and_notify "$message" "error"
     exit 1
   fi
+
   get_current_brightness
   if [ "$user_param" = "increase" ]; then
     if [ "$current_brightness" -ge 90 ]; then
@@ -48,14 +80,13 @@ process_input() {
     new_brightness="$user_param"
   else
     message="Invalid parameter. It must be either 'increase', 'decrease', or a number from 30 to 100"
-    echo -e "\n${RED}$message${NC}"
-    notify-send "Brightness Control - Error" "$message"
+    log_and_notify "$message" "error"
     exit 1
   fi
-  echo "current_brightness: $current_brightness"
-  echo "new_brightness: $new_brightness"
+  log "current_brightness: ${BLUE}$current_brightness${NC}"
+  log "new_brightness: ${BLUE}$new_brightness${NC}"
   if [ "$current_brightness" = "$new_brightness" ]; then
-    notify-send "Brightness Control" "Left brightness as is since it was at $current_brightness% and new brightness was $new_brightness"
+    log_and_notify "Left brightness as is since it was at $current_brightness% and new brightness was $new_brightness%"
     return
   else
     set_brightness "$new_brightness"
@@ -63,11 +94,10 @@ process_input() {
 
   result=$?
   if [ $result -eq 0 ]; then
-    notify-send "Brightness Control" "Adjusted external display brightness from $current_brightness% to $new_brightness%"
+    log_and_notify "Adjusted external display brightness from $current_brightness% to $new_brightness%"
   else
     message="Encountered error setting external display brightness to ${new_brightness}%"
-    echo -e "$message:\n\n$output"
-    notify-send "Brightness Control - Error" "$message\n\n$output"
+    log_and_notify "$message\n\n$output" "error"
   fi
 }
 
@@ -83,5 +113,7 @@ get_current_brightness() {
   current_brightness="${BASH_REMATCH[1]}"
 }
 
-user_param=$1
+# Main script
+user_param="$1"
+log "Running brightness control script with parameter: ${BLUE}$user_param${NC}"
 process_input
